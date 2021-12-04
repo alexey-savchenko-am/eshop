fdf5d8030ef2c6aff730ccccc8853a8e822d3056
Alexey <alexey.repository@gmail.com>
2021-12-04 23:33:37 +0300

    hide cart menu after jumping to checkout page & styling checkout page

--- src/redux/cart/cart-selectors.js
+++ src/redux/cart/cart-selectors.js
@@ -17,3 +17,8 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>  cartItems.reduce((q, i) => q + i.quantity, 0)
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce((q, i) => q + i.quantity * i.price, 0)
+);
